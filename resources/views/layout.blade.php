2dba760ba3dd4fe5217348e589708525f1b6dc35
曵田剛 <go@gous-MacBook-ea.local>
2019-11-02 11:55:25 +0900

    ok

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel BBS</title>
+
+    <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+    >
+</head>
+<body>
+    <header class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('bbs') }}">
+                Laravel BBS
+            </a>
+
+
+
+        </div>
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+           @csrf
+         </form>
+    </header>
+
+    <div>
+        @yield('content')
+    </div>
+</body>
+@if(Auth::check())
+  <script>
+    document.getElementById('logout').addEventListener('click', function(event) {
+      event.preventDefault();
+      document.getElementById('logout-form').submit();
+    });
+  </script>
+@endif
+</html>
